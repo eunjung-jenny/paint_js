b39831e5f35f4cf2ec1d9b10a0a2365f6a7039fb
jenny.kr <jenny.it.kr@gmail.com>
2019-11-17 01:13:24 +0900

    Add mode change function

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const canvas = document.querySelector(".js-canvas");
 const ctx = canvas.getContext("2d");
+const mode = document.querySelector(".js-mode");
 
 canvas.width = 550;
 canvas.height = 550;
 
 let painting = false;
+let filling = false;
+
+function handleModeChange(event) {
+  if (filling) {
+    event.target.innerHTML = "paint";
+  } else {
+    event.target.innerHTML = "fill";
+  }
+  filling = !filling;
+}
 
 function stopPainting(event) {
   painting = false;
 }
 
 function onMouseDown(event) {
-  painting = true;
+  if (filling) {
+    ctx.fillRect(0, 0, 550, 550);
+  } else {
+    painting = true;
+  }
 }
 
 function onMouseMove(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
   if (painting) {
-    ctx.lineTo(x, y);
-    ctx.stroke();
-  } else {
-    ctx.moveTo(x, y);
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (painting) {
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    } else {
+      ctx.moveTo(x, y);
+    }
   }
 }
 
@@ -30,4 +47,8 @@ if (canvas) {
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("mouseup", stopPainting);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeChange);
 }
