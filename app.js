046152184c6f4b0fadc298a412d56f2d354fbf3c
jenny.kr <jenny.it.kr@gmail.com>
2019-11-17 00:57:21 +0900

    Fix drawing function on  mouse events

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ const ctx = canvas.getContext("2d");
 canvas.width = 550;
 canvas.height = 550;
 
+let painting = false;
+
+function stopPainting(event) {
+  painting = false;
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  ctx.lineTo(x, y);
-  ctx.stroke();
+  if (painting) {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  } else {
+    ctx.moveTo(x, y);
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("mouseup", stopPainting);
 }
